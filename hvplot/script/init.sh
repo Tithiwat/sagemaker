a81d87c9b34c65c104397c82fb37003e0d19f9a6
EC2 Default User <ec2-user@ip-172-16-82-64.ap-southeast-1.compute.internal>
2023-02-10 07:28:20 +0000

    update script

--- hvplot/script/init.sh
+++ hvplot/script/init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #eval "$(command conda 'shell.bash' 'hook' 2> /dev/null)"
 source ~/anaconda3/bin/activate
-wget https://raw.githubusercontent.com/Tithiwat/sagemaker/main/script/geo-notebook-element84.yml
+wget https://raw.githubusercontent.com/Tithiwat/sagemaker/main/hvplot/script/geo-notebook-element84.yml
 conda env create -y -f geo-notebook-element84.yml
 conda activate odc-geonotebook
 conda install -c anaconda ipykernel
