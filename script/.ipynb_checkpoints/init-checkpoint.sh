bf06ab9e6d9c21e2649f0e51aa69b46655303cc6
EC2 Default User <ec2-user@ip-172-16-79-217.ap-southeast-1.compute.internal>
2023-02-10 02:24:54 +0000

    Add .gitignore

--- script/.ipynb_checkpoints/init-checkpoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#eval "$(command conda 'shell.bash' 'hook' 2> /dev/null)"
-source ~/anaconda3/bin/activate
-conda env create -y -f geo-notebook-element84.yml
-conda activate odc-geonotebook
-conda install -c anaconda ipykernel
